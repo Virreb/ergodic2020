49ea61cf9249dc52296e67f424a2a35cab238428
Mattias Wikstrom <mattias.wikstrom@advectas.se>
2020-09-30 20:57:34 +0200

    Add get_aggregate_state

--- main.py
+++ main.py
@@ -67,6 +67,119 @@ def end_games():
     print('dsasd')
 
 
+
+def get_aggregated_state(state):
+    aggregated_state = {}
+
+    # Funds
+    if state.funds >= 40000:
+        funds = "HIGH"
+    elif 10000 < state.funds < 40000:
+        funds = "MEDIUM"
+    else:
+        funds = "LOW"
+    aggregated_state["funds"] = funds
+
+
+    # Queue
+    if state.housing_queue >= 30:
+        housing_queue = "HIGH"
+    elif 15 < state.housing_queue < 30:
+        housing_queue = "MEDIUM"
+    else:
+        housing_queue = "LOW"
+    aggregated_state["housing_queue"] = housing_queue
+
+
+    # Queue Happiness
+    if state.queue_happiness >= 30:
+        queue_happiness = "HIGH"
+    elif 15 < state.queue_happiness < 30:
+        queue_happiness = "MEDIUM"
+    else:
+        queue_happiness = "LOW"
+    aggregated_state["queue_happiness"] = queue_happiness
+
+
+    # Population Capacity
+    current_pop = 0
+    max_capacity = 0
+    for resident in state.residences:
+        current_pop = current_pop + resident.current_pop
+        bp = game_layer.get_residence_blueprint(resident.building_name)
+        max_capacity = max_capacity + bp.max_pop
+    pop_capacity = max_capacity - current_pop
+    if pop_capacity >= 30:
+        pop_capacity = "HIGH"
+    elif 5 < pop_capacity < 30:
+        pop_capacity = "MEDIUM"
+    else:
+        pop_capacity = "LOW"
+    aggregated_state["pop_capacity"] = pop_capacity
+
+    # Max Diff Energy
+    max_energy_diff = 0
+    actual_diff = 0
+    for resident in state.residences:
+        current_diff = resident.requested_energy_in - resident.effective_energy_in
+        if abs(current_diff) > max_energy_diff:
+            max_energy_diff = abs(current_diff)
+            actual_diff = current_diff
+
+    if actual_diff >= 5:
+        actual_diff = "HIGH"
+    elif 1 < actual_diff < 5:
+        actual_diff = "MEDIUM"
+    else:
+        actual_diff = "LOW"
+    aggregated_state["max_energy_diff"] = actual_diff
+
+    # Min Happiness Pop Tick
+    min_happiness_per_tick_pop = None
+    for resident in state.residences:
+        # For the first building, set the min_happiness
+        if min_happiness_per_tick_pop is None:
+            min_happiness_per_tick_pop = resident.happiness_per_tick_per_pop
+        # For the rest of the buildings, pick the lowest.
+        if resident.happiness_per_tick_per_pop < min_happiness_per_tick_pop:
+            min_happiness_per_tick_pop = resident.happiness_per_tick_per_pop
+    aggregated_state["min_happiness_per_tick_pop"] = min_happiness_per_tick_pop
+
+    # Min Building Health
+    building_health = []
+    for resident in state.residences:
+        building_health.append(resident.health)
+    if len(building_health) > 0:
+        if min(building_health) > 80:
+            building_health = "HIGH"
+        elif 40 < min(building_health) < 80:
+            building_health = "MEDIUM"
+        else:
+            building_health = "LOW"
+    else:
+        building_health = "LOW"
+    aggregated_state["building_health"] = building_health
+
+    # Max Temp Diff
+    max_temp_diff = 0
+    actual_diff = 0
+    for resident in state.residences:
+        current_diff = 21 - resident.temperature
+        if abs(current_diff) > max_temp_diff:
+            max_temp_diff = abs(current_diff)
+            actual_diff = current_diff
+
+    if actual_diff >= 3:
+        actual_diff = "HOT"
+    elif -3 < actual_diff < 3:
+        actual_diff = "LAGOM"
+    else:
+        actual_diff = "COLD"
+    aggregated_state["max_temp_diff"] = actual_diff
+
+    return aggregated_state
+
+
 if __name__ == "__main__":
     main()
     #end_games()
